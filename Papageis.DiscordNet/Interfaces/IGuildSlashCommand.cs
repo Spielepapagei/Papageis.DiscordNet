b243c40e6b2603d1ba289573c8ba989655b000c2
Ole Sziedat <info@spielepapagei.de>
2024-12-27 17:02:34 +0100

    Implementet User/Message Commands

--- Papageis.DiscordNet/Interfaces/IGuildSlashCommand.cs
+++ Papageis.DiscordNet/Interfaces/IGuildSlashCommand.cs
@@ -1,9 +1,11 @@
+using Discord.Interactions.Builders;
+
 namespace Papageis.DiscordNet.Module;
 
 public interface IGuildSlashCommand : IBaseSlashCommand
 {
     /// <summary>
-    /// Set the Guild where the Command should be Registered
+    /// Set the Guild where the SlashCommand should be Registered
     /// </summary>
     public Task<ulong> GuildId();
 }
